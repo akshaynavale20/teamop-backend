53665dc4f34703eef13831c01ca8b6ec31192bc1
Akshay <akshay.navale@ext.zestmoney.in>
2022-01-15 22:40:58 +0530

    added host

--- src/setup/settings.py
+++ src/setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fm(6pb-%e86=3w=esd2q9nnx2%6g&u6t7n9m548*-8bwl9qv0v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',"3.134.244.83"]
 
 
 # Application definition
