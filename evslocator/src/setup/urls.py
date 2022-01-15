1e8a9c96c913d59ff120bc8b24d990c78cd4d2ef
Akshay <akshay.navale@ext.zestmoney.in>
2022-01-15 10:58:49 +0530

    Added hello world reponse
    added health check route

--- evslocator/src/setup/urls.py
+++ evslocator/src/setup/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from evslocator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health/', views.index, name='index'),
 ]
